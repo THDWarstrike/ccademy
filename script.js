bf7b718f6feb0617a43ceee8cfb6e1fc08552eba
THDWarstrike <89410413+THDWarstrike@users.noreply.github.com>
2022-02-28 19:21:33 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+var input = "whale talk"
+var e = ""
+var text = ""
+var vowels = ['a','e', 'i', 'o', 'u']
+const resultArray = []
+
+for (let i = 0; i < input.length; i++) {
+  if(input == e){
+    text.push(1)
+      if(input == u){
+    text.push(4)
+    }
+  }
+  for(let j = 0; j < vowels.length; j++){ 
+    console.log('j is '+ j) 
+}}
+console.log(resultArray)
